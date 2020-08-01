713447ab1eaacf7b16f0109e29fee477a4caf604
funglabo <68882085+funglabo@users.noreply.github.com>
2020-08-01 02:49:12 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,10 +1,10 @@
 <?php
-$destino="edgarhuerta15@gmail.com";
+$destino = "edgarhuerta15@gmail.com";
 $nombre = $_POST["nombre"];
 $correo = $_POST["e-mail"];
 $telefono = $_POST["telefono"];
 $mensaje = $_POST["mensaje"];
 $contenido = "nombre: " . $nombre . "\nCorreo: " . $correo . "\ntelefono: " . $telefono . "\nMensaje: " . $mensaje;
 mail($destino, "contacto", $contenido);
-header("location: index.html");
+header("location:index.html");
 ?>
